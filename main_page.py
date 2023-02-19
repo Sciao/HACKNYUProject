e134704305ff0505f8d9a2b5a3c7969719c22007
Sciao <islapphosschool@gmail.com>
2023-02-18 23:18:25 -0500

    formatting fix for the form

--- main_page.py
+++ main_page.py
@@ -1,22 +1,27 @@
 import streamlit as st
-import dataset as ds
+import pages.dataset as ds
 
-st.markdown("# Main page 🎈")
-st.sidebar.markdown("# Main page 🎈")
+st.markdown("All You Need for Mental Health Help")
+st.sidebar.markdown("Home")
 
-st.write("Welcome to the Extra Mental Health Resources \n Please input your information below.")
+st.write("Welcome to the Extra Mental Health Resources. To be best matched with avaliable resources, please input your information below.")
 
 with st.form("user_input_form"):
-   st.write("Please input your information accurately.")
-   zip_code_info = st.number_input("Your zipcode:", 9999,15000, value = 10003, key = "zipcode")
-   medium_preference = st.selectbox("Pick your preferred medium:", ["Virtual", "In-person"], key = "medium")
+   st.write("Please fill in the following fields accurately")
+
+   age_info = st.number_input("Please indicate your age. If you are underage, please seek assistance from your parent or legal guardian", min_value=0)
+
+   issue_info = st.multiselect("Please select what issues you are having:", ["Depression", "Anxiety", "Stress", "Dietary Concerns"], key = "issue")
+
+   zip_code_info = st.number_input("What is your geographic location by zipcode? (NY is in the range of 10000-15000)", 9999,15000, value = 9999, key = "zipcode")
+
+   medium_preference = st.selectbox("Please select your preferred sesssion medium:", ["Virtual", "In-person"], key = "medium")
+
    insurance_provider = st.text_input("Type in your insurance provider's name (Leave Blank if not insured)", key = "insurance")
-   issue_info = st.multiselect("What issues plague you?", ["Depression", "Anxiety", "Stress", "Dietary Concerns"], key = "issue")
 
    assent_checkbox_val = st.checkbox("By submitting, you agree that your information will only be used for searching for resources and will not be saved.", value=False, key = "acceptance")
-
+   
    submitted = st.form_submit_button("Submit")
-
 
 if submitted:
    ds.get_clinic_data(zipcode = st.session_state.zipcode);
